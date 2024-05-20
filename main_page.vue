47170693b6b1bfe13d50323d4262aa9b01810fd3
HuanLinOTO <linsmc@126.com>
2024-05-20 10:11:39 +0800

    open new guild

--- main_page.vue
+++ main_page.vue
@@ -55,8 +55,9 @@
                     </n-space>
                 </n-space>
                 <n-space vertical>
-                    <n-space class="group-item disable">
-                        <n-badge value="暂未开放">
+                    <n-space class="group-item"
+                        @click="goto('https://qm.qq.com/cgi-bin/qm/qr?k=Z93WMbn5a8v6MXDiBzcywyEFcIdhi_ls&jump_from=webapi&authKey=tCNl9eJLLVY2TNgf2KyT2cWylxNMAlWxScUHIVqc2bz0xbSf05ZMLR4ooq0yGcim')">
+                        <n-badge value="新群">
                             <n-avatar :size="48" src="https://p.qlogo.cn/gh/172701496/172701496/40" />
                         </n-badge>
                         幻灵的炼丹工坊二群
