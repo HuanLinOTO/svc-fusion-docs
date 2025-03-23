548519c1b65377a229a5eeffdd695843c0c9fa50
HuanLinOTO <linsmc@126.com>
2025-03-23 07:41:58 +0000

    Deploying to gh-pages from @ HuanLinOTO/svc-fusion-docs@0f58ccf79a413b589cb92ac166ff07715eebd3e5 🚀

---
+++ assets/start_linux.md.CXUS32ap.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as e,o as t,V as o}from"./chunks/framework.BpLZpJbo.js";const x=JSON.parse('{"title":"关于 Linux","description":"","frontmatter":{},"headers":[],"relativePath":"start/linux.md","filePath":"start/linux.md"}'),n={name:"start/linux.md"},i=o("",16),r=[i];function s(c,l,d,h,p,u){return t(),e("div",null,r)}const m=a(n,[["render",s]]);export{x as __pageData,m as default};
